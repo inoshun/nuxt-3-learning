eef59ba1323ae185b983183b5c9fdbe6e2bef3a6
Shun Inoue <shun.inoue11@gmail.com>
2024-01-22 21:37:42 +0900

    First commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@nuxtjs/eslint-config-typescript",
+    "plugin:prettier/recommended",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "@typescript-eslint/no-unused-vars": "error",
+    "@typescript-eslint/no-explicit-any": "error",
+  },
+};
---
+++ components/SampleComponent.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-4xl text-red-500">This is Sample Component</div>
+</template>
